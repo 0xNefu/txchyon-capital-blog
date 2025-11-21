21a66661af2c2e58b9c7008d74e9eae58ed47b88
Nefu <55779050+0xNefu@users.noreply.github.com>
2025-11-21 05:17:59 -0800

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,1 +1,15 @@
+import { z, defineCollection } from 'astro:content';
 
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: http://z.date(),
+    updatedDate: http://z.date().optional(),
+    heroImage: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = { blog };
